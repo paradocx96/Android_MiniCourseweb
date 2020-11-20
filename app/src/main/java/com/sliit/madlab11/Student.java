ca039b31140527a4be76ed079472659c9d8bcfa2
Navinda Lankesh <47824016+paradocx96@users.noreply.github.com>
2020-11-20 13:44:31 +0530

    Notification function implementation done!

--- app/src/main/java/com/sliit/madlab11/Student.java
+++ app/src/main/java/com/sliit/madlab11/Student.java
@@ -3,18 +3,24 @@ package com.sliit.madlab11;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.Intent;
+import java.util.ArrayList;
+import android.os.Build;
 import android.os.Bundle;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import android.content.Intent;
+import android.app.PendingIntent;
 import android.widget.TextView;
 
 import com.sliit.madlab11.database.DBHandler;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Student extends AppCompatActivity {
 
+    private static final String CHANNEL_ID = "0001";
     TextView txt_studentName;
     String studentName;
     RecyclerView recyclerView;
@@ -38,6 +44,8 @@ public class Student extends AppCompatActivity {
         recyclerView.setAdapter(customAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(Student.this));
 
+        createNotificationChannel();
+
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
         if (bundle != null){
@@ -52,5 +60,51 @@ public class Student extends AppCompatActivity {
         users = (ArrayList<String>) dbHandler.readAllMessage("User");
         subjects = (ArrayList<String>) dbHandler.readAllMessage("Subject");
         messages = (ArrayList<String>) dbHandler.readAllMessage("Message");
+    }
+
+    private void createNotificationChannel() {
+        String un, msg, sub;
+
+        un = users.get(0);
+        sub = subjects.get(0);
+        msg = messages.get(0);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        // Create an explicit intent for an Activity in your app
+        Intent intent = new Intent(this, Message.class);
+
+        intent.putExtra("user",un);
+        intent.putExtra("subject",sub);
+        intent.putExtra("message",msg);
+
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("You have a new Message")
+                .setContentText("Click here to view latest message!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(0, builder.build());
     }
 }
